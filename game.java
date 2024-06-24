56be08c25da507c6ff5aa9e4e8b61c1fe105f738
Nikhiltyagi008 <154893464+Nikhiltyagi008@users.noreply.github.com>
2024-06-24 09:05:47 +0530

    Add files via upload

---
+++ game.java
@@ -0,0 +1,41 @@
+// import java .util.Scanner;
+// import java .util. Random;
+// public class game{
+//     public static void main(String[] args){
+//         // int rock=0;
+//         // int paper=1;
+//         // int scissor=2;
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter the input");
+//         int userinput=sc.nextInt();
+//         Random rnd=new Random();
+//         int computerinput=rnd.nextInt(3);
+//         if(computerinput==userinput){
+//             System.out.println("Draw or no one wins ");
+
+//         }else if(computerinput==0&&userinput==1||computerinput==1&&userinput==2||computerinput==2&&userinput==0){
+//             System.out.println("user win");
+//         }
+//         else{
+//             System.out.println("computer wins");
+//         }
+//         System.out.println("computer choices is"+computerinput);
+//         if(computerinput==0){
+//             System.out.println("computer chose rock");
+//         }
+//         else if(computerinput==1){
+//             System.out.println("computer choice is paper");
+//         }
+//         else{
+//             System.out.println("computer choices is scissor");
+//         }
+//         sc.close();
+//     }
+// }
+public class game{
+    public static void main(String[] args) {
+        while(true){
+            System.out.println();
+        }
+    }
+}
